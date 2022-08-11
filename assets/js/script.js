dfc25a0b53a59bcca3eff5318765974b494c78fe
DTGuy <tguy5837@gmail.com>
2022-08-11 13:22:16 -0400

    added promts for pw criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var userInput = {
+    characters: window.prompt("How many characters?"),
+    lowercase: window.confirm("Click 'Confirm' to include lowercase letters."),
+    uppercase: window.confirm("Click 'Confirm' to include uppercase letters."),
+    numbers: window.confirm("Click 'Confirm' to include numbers."),
+    special: window.confirm("Click 'Confirm' to include special characters."),
+  };
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
