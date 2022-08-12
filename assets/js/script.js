f6e5fd62175b0e763597e30acee86ed8419ac350
DTGuy <tguy5837@gmail.com>
2022-08-12 12:42:25 -0400

    added generate password functionality and returned result

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character possibilities:
+var lowerCaseSet = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCaseSet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numberSet = ["0123456789"];
+var specialSet = ["!@#$%&*_-<>/"];
+
+
 // Generate Password function
 function generatePassword() {
-  // 1. Prompt user for password criteria
-  //    a. Length between 8 - 128
-  //    b. Ask about special characters & everything else
+  // Clear old generating data
+  var generatedPW = "";
+  var generateOptions = "";
+
+
+  // 1. Prompt user for password length (length between 8-128)
 
   var characters = window.prompt(
     "How many characters? Must be between 8 and 128."
@@ -14,91 +24,49 @@ function generatePassword() {
     characters = window.prompt("Please choose a value between 8 and 128.");
   }
 
-  function charConfirm() {
-    var lowerCase = window.confirm(
-      "Click 'Confirm' to include lowercase letters."
+  // 2. Prompt use for other password criteria (must choose at least one)
+  var lowerCase = window.confirm("Click 'Confirm' to include lowercase letters.");
+  var upperCase = window.confirm("Click 'Confirm' to include uppercase letters.");
+  var numbers = window.confirm("Click 'Confirm' to include numbers.");
+  var special = window.confirm("Click 'Confirm' to include special characters.");
+
+  // 3. Validate the input
+  while (!lowerCase && !upperCase && !numbers && !special) {
+    window.alert(
+      "Please select 'OK' for at least one of the previous 4 options."
     );
-    var upperCase = window.confirm(
-      "Click 'Confirm' to include uppercase letters."
-    );
+    var lowerCase = window.confirm("Click 'Confirm' to include lowercase letters.");
+    var upperCase = window.confirm("Click 'Confirm' to include uppercase letters.");
     var numbers = window.confirm("Click 'Confirm' to include numbers.");
-    var special = window.confirm(
-      "Click 'Confirm' to include special characters."
-    );
-
-    // 2. Validate the input
-    if (!lowerCase && !upperCase && !numbers && !special) {
-      window.alert(
-        "Please select 'OK' for at least one of the previous 4 options."
-      );
-      charConfirm();
-    }
+    var special = window.confirm("Click 'Confirm' to include special characters.");
   }
-  charConfirm();
-  // 3. Generate password based on criteria
+  console.log(lowerCase, upperCase, numbers, special);
 
-  // 4. Display generated password on page}
+  // 3. Use input to create a string that includes only confirmed criteria
+  if (lowerCase) {
+    generateOptions += lowerCaseSet;
+  }
+
+  if (upperCase) {
+    generateOptions += upperCaseSet;
+  }
+
+  if (numbers) {
+    generateOptions += numberSet;
+  }
+
+  if (special) {
+    generateOptions += specialSet;
+  }
+
+  // 4. Generate password
+  for (var i = 0; i < characters; i++) {
+    generatedPW += generateOptions.charAt(Math.floor(Math.random() * generateOptions.length));
+  }
+
+  // 4. Display generated password on page
+  return generatedPW;
 }
-
-// Character possibilities:
-var lowerCaseSet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var upperCaseSet = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var numberSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialSet = ["!", "@", "#", "$", "%", "&", "*", "_", "-", "<", ">", "/"];
 
 // Write password to the #password input
 function writePassword() {
